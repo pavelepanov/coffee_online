356833ba63078d4c4ca7491652ef06fb694d7a34
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2024-11-11 16:37:52 +0500

    Add domain adress

---
+++ src/coffee_online/domain/entities/address.py
@@ -0,0 +1,16 @@
+from coffee_online.domain.common.entity import Entity
+from coffee_online.domain.value_objects.address_id import AddressId
+from coffee_online.domain.value_objects.address_title import AddressTitle
+
+
+class Address(Entity):
+    def __init__(self, id: AddressId, title: AddressTitle) -> None:
+        super().__init__(id=id)
+        self.title = title
+
+
+def address_factory(id: int, title: str) -> Address:
+    return Address(
+        id=AddressId(id),
+        title=AddressTitle(title),
+    )
---
+++ src/coffee_online/domain/value_objects/address_id.py
@@ -0,0 +1,23 @@
+import logging
+
+from coffee_online.domain.common.errors import ValueObjectValidationError
+from coffee_online.domain.common.value_object import ValueObject
+
+logger = logging.getLogger(__name__)
+
+
+class AddressId(ValueObject):
+    def __init__(self, id: int):
+        self.__id = id
+        self._validate()
+
+    def _validate(self) -> None:
+        try:
+            if not isinstance(self.__id, int):
+                raise ValueObjectValidationError("Address id must be an int")
+        except Exception as e:
+            logging.exception("Address id did not create because %s" % e)
+
+    @property
+    def id(self):
+        return self.__id

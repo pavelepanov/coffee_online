19198d96e11f0524923528a016228731ddadaed9
Pavel Epanov <pavelepanov@MacBook-Air-Pavel.local>
2024-11-11 16:56:32 +0500

    Fix after time

---
+++ src/coffee_online/domain/entities/order.py
@@ -0,0 +1,6 @@
+from coffee_online.domain.common.entity import Entity
+
+
+class Order(Entity):
+    def __init__(self):
+        super().__init__()
---
+++ src/coffee_online/domain/value_objects/after_time.py
@@ -0,0 +1,26 @@
+import logging
+
+from coffee_online.domain.common.errors import ValueObjectValidationError
+from coffee_online.domain.common.value_object import ValueObject
+
+logger = logging.getLogger(__name__)
+
+
+class AfterTime(ValueObject):
+    def __init__(self, time: int):
+        self.__time = time
+        self._validate()
+
+    def _validate(self) -> None:
+        times = [5, 10, 15, 30]
+        try:
+            if not isinstance(self.__time, int):
+                raise ValueObjectValidationError("Time must be an int")
+            if self.__time not in times:
+                raise ValueObjectValidationError("Time must be in [5, 10, 15, 30] minutes")
+        except Exception as e:
+            logging.exception("Time did not create because %s" % e)
+
+    @property
+    def time(self):
+        return self.__time
